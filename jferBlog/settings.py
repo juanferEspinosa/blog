ba0b2eea9ee0dd0ebe13a8e2a0ae697c4cdc82ab
juanEspinosa <espinosajfer@gmail.com>
2024-05-12 14:09:12 +0200

    add whitenoise settings

--- jferBlog/settings.py
+++ jferBlog/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'whitenoise.runserver_nostatic',
     'homepage.apps.HomepageConfig',
     'blog.apps.BlogConfig',
     'django.contrib.sitemaps',
@@ -60,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
