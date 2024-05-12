e9e8571a1b56ebb3643f2bcd351842b20cbd3dc6
juanEspinosa <espinosajfer@gmail.com>
2024-05-12 14:15:12 +0200

    commit

--- jferBlog/settings.py
+++ jferBlog/settings.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') #it is telling django where to save the uploaded images by users.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
