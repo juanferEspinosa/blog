56f35508e6ce6912976b18cb811b5960ee918e24
juanEspinosa <espinosajfer@gmail.com>
2024-05-19 17:36:19 +0200

    add username blacklist

--- jferBlog/settings.py
+++ jferBlog/settings.py
@@ -167,3 +167,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+ACCOUNT_USERNAME_BLACKLIST = ['admin', 'accounts', 'profile', 'category', 'post','management']
