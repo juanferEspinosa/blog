d0c5303d78e78f29192bfc7a01a5a7d08a9cb82e
juanEspinosa <espinosajfer@gmail.com>
2024-08-07 08:38:11 +0200

    Update chroma database

--- blog/llm_logic.py
+++ blog/llm_logic.py
@@ -14,7 +14,7 @@ from langchain_openai import ChatOpenAI
 from typing import Union, Optional, Any
 
 
-CHROMA_DB_DIRECTORY = "chroma_db/docs_builder"
+CHROMA_DB_DIRECTORY = "chroma_db"
 SCRAPER_LINK_LIMIT = 100
 
 
@@ -46,9 +46,9 @@ class CustomWebBaseLoader(WebBaseLoader):
         # Fetching the HTML content using the parent class method
         html_content = super()._scrape(url, parser)
 
-        main_tag = html_content.find('div')  # Find the <main> tag
+        main_tag = html_content.find('div')  
         # return the text in the div tag, parsed by bs4
-        return BeautifulSoup(main_tag.text, "html.parser", **(bs_kwargs or {})) # Here we’re overriding the basic scraper to take only the contents from the main tag, with a couple of lines we’re avoiding already a lot of noise that we would otherwise get in the scrape.
+        return BeautifulSoup(main_tag.text, "html.parser", **(bs_kwargs or {})) # Here we’re overriding the basic scraper to take only the content we want.
     
 # Check if the database already exists
 def database_exists():
@@ -58,11 +58,11 @@ def build_database():
     urls = docs_builder()
     loader = CustomWebBaseLoader(urls)
     documents = loader.load()
-    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=1000, chunk_overlap=0)
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=1000, chunk_overlap=2)
     splits = splitter.split_documents(documents)
     # Initialize openAI embeddings
     embeddings = OpenAIEmbeddings()
-    # Build Chroma database with the documents already split and embedded.
+    # Build Chroma database with the documents embedded.
     db = Chroma.from_documents(
         splits,
         embeddings,
@@ -70,14 +70,15 @@ def build_database():
         persist_directoty=CHROMA_DB_DIRECTORY,
 
     )
-    # DB persist save the data into the database making it available for future use without having to rebuild.
+    # DB persist save the data into the database making it available for future use.
     db.persist()
+    print(f"Saved {len(splits)} chunks to {CHROMA_PATH}.")
 
-# Need a function to answer the query after a lookup in the chunk of documents
+
 def answer_query(query):
-    # Get vector representationof the user question
+    # Get vector representation of user question
     embeddings = OpenAIEmbeddings()
-    # Fetch the vector collection to compare against the question
+    # Get own embedding to apply similarity
     db = Chroma(
         collection_name="doc_builder",
         embedding_function=embeddings,
@@ -89,8 +90,9 @@ def answer_query(query):
         # type of GPT version -- gtp3.5
         llm=chat,
         chain_type="stuff",
-        retriever=db.as_retriever(), # Use the chroma DB to check for results
-        chain_type_kwargs={"verbose": True}  # Log everything
+        #Checking chroma DB to see results
+        retriever=db.as_retriever(),
+        chain_type_kwargs={"verbose": True}  # Log
 
     )
     result = chain({"question":query}, return_only_outputs=True)
