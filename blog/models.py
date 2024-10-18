65662c28ca7018e864f75a8bd445348a1cfc6262
juanEspinosa <espinosajfer@gmail.com>
2024-10-18 15:02:58 +0200

    Add tag into model

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     class Meta:
         ordering = ['-created']
@@ -19,3 +20,11 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return self.slug
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
