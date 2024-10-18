1ced1c91aab64b064d1e0947100722fd674f8d53
juanEspinosa <espinosajfer@gmail.com>
2024-10-18 15:49:29 +0200

    Erase Tag model and add tag field

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,7 @@
 from django.db import models
 import uuid
 
-class Tag(models.Model):
-    name = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
-    def __str__(self):
-        return self.name
 
 
 class Post(models.Model):
@@ -19,7 +13,7 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    tags = models.ManyToManyField('Tag', blank=True,null=True)
+    tags = models.CharField(max_length=400, null=True, blank=True)
 
     class Meta:
         ordering = ['-created']
