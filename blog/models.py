65d8e7817b38e6967314f67b44be900b2e5a11b3
juanEspinosa <espinosajfer@gmail.com>
2024-10-18 18:18:29 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    tag = models.CharField(max_length=400, null=True, blank=True)
+    tag = models.CharField(max_length=300, null=True, blank=True)
 
 
     class Meta:
