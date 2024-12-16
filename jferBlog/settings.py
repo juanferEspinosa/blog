20a542aecee666e748d32aad3943df2b9f902168
juanEspinosa <espinosajfer@gmail.com>
2024-12-16 21:20:34 +0100

    reverse database connection

--- jferBlog/settings.py
+++ jferBlog/settings.py
@@ -96,12 +96,12 @@ WSGI_APPLICATION = 'jferBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 """DATABASES = {
     'default': {
